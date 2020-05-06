a5af849168985b6a62e0ec70d4e74322b8d22875
Piotr <murzakoy2326@gmail.com>
2020-05-06 23:11:11 +0300

    Added infinity platform

--- core/src/com/unicorngames/dodgethis/Level1.java
+++ core/src/com/unicorngames/dodgethis/Level1.java
@@ -7,24 +7,50 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
 
 public class Level1 {
-    Texture floor;
-    Vector2 floorpos;
-    int speed = 3;
+    class Platform {
+
+        private Texture tx;
+        private Vector2 pos;
+
+        public Platform(Vector2 pos) {
+            tx = new Texture("grass_platform.png");
+            this.pos = pos;
+        }
+    }
+
+    private int speed;
+    Platform[] platform;
 
     public Level1() {
-        floor = new Texture("floor.png");
-        floorpos = new Vector2(0 ,215);
+        speed = 3;
+        platform = new Platform[3];
+        platform[0] = new Platform(new Vector2(-1000,200));
+        platform[1] = new Platform(new Vector2(0,200));
+        platform[2] = new Platform(new Vector2(1000,200));
     }
+
     public void render(SpriteBatch batch) {
-        batch.draw(floor, floorpos.x, floorpos.y);
+        for (int i = 0; i <platform.length; i++) {
+            batch.draw(platform[i].tx, platform[i].pos.x, platform[i].pos.y);
+        }
     }
 
     public void update() {
         if (Gdx.input.isKeyPressed(Input.Keys.A)) {
-            floorpos.x += speed;
+            for (int i = 0; i < platform.length; i++) {
+                platform[i].pos.x += speed;
+            }
         }
         if (Gdx.input.isKeyPressed(Input.Keys.D)) {
-            floorpos.x -= speed;
+            for (int i = 0; i < platform.length; i++) {
+                platform[i].pos.x -= speed;
+            }
+        }
+
+        if (platform[1].pos.x > 1000 || platform[1].pos.x < -1000) {
+            platform[0].pos.x = -1000;
+            platform[1].pos.x = 0;
+            platform[2].pos.x = 1000;
         }
     }
 }
