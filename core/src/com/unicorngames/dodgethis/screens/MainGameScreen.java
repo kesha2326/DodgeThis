d3fff972530262003b707b50936f0e6492ff2f42
Piotr <murzakoy2326@gmail.com>
2020-05-08 12:52:02 +0300

    Added experimental gameplay

--- core/src/com/unicorngames/dodgethis/screens/MainGameScreen.java
+++ core/src/com/unicorngames/dodgethis/screens/MainGameScreen.java
@@ -1,16 +1,24 @@
 package com.unicorngames.dodgethis.screens;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.assets.loaders.I18NBundleLoader;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
 import com.unicorngames.dodgethis.DodgeThis;
 
 public class MainGameScreen implements Screen {
 
     DodgeThis dodgeThis;
 
+    Texture tx;
+    public int cubex = 0;
+    public int cubey = 0;
+
     public MainGameScreen(DodgeThis dodgeThis) {
         this.dodgeThis = dodgeThis;
+        tx = new Texture("snake.jpg");
     }
 
     @Override
@@ -25,6 +33,22 @@ public class MainGameScreen implements Screen {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         dodgeThis.batch.begin();
+
+        dodgeThis.batch.draw(tx, cubex, cubey);
+
+        if (Gdx.input.isKeyPressed(Input.Keys.W) && cubey < dodgeThis.HEIGHT - 30) {
+            cubey += 4;
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.S) && cubey > 0) {
+            cubey -= 4;
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.A) && cubex > 0) {
+            cubex -= 4;
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.D) && cubex < dodgeThis.WIDTH - 30) {
+            cubex += 4;
+        }
+
         dodgeThis.batch.end();
     }
 
